d529b6731103d126508e902a256aa0e9bb473479
Aman Shankar Mahapatra <amanshankarmahapatra@gmail.com>
2026-09-10 20:25:49 +0530

    -1

--- AI agent/tempCodeRunnerFile.py
+++ AI agent/tempCodeRunnerFile.py
@@ -1,6 +1,6 @@
 from google import genai
 
-client = genai.Client(api_key="AQ.Ab8RN6IkY9jL1IVqse6CYdoJtWF5bZOWzOLrjaX7b13g8TRmAg")
+client = genai.Client(api_key="APT_KE")
 
 for model in client.models.list():
     print(model.name)
