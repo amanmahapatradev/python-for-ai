7d10c4e81b60fc565faf509bb35cadd5384e287e
Aman Shankar Mahapatra <amanshankarmahapatra@gmail.com>
2026-09-13 14:44:25 +0530

    C-2

--- AI agent/programming_types/pydaaantic.py
+++ AI agent/programming_types/pydaaantic.py
@@ -1,1 +1,11 @@
-import pydantic 
+from pydantic import BaseModel
+
+class PatientData(BaseModel):
+    name: str
+    age: int
+    
+def add_patient_data():
+    
+    name: "Soumya"
+    age: 25
+    
