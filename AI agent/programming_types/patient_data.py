cf6dfaaed1e0e0fa717b834fb4b4d1c0734eeb1d
Aman Shankar Mahapatra <amanshankarmahapatra@gmail.com>
2026-09-14 09:58:26 +0530

    Commit-1

--- AI agent/programming_types/patient_data.py
+++ AI agent/programming_types/patient_data.py
@@ -212,3 +212,57 @@ patient_data_1 = {
 patient_1 = PatientData(**patient_data_1)
 
 add_patient_data(patient_1)
+#------------------------------------------------------------------------
+
+#Field validator 
+
+from pydantic import BaseModel, EmailStr, AnyUrl, Field, field_validator
+from typing import List,Dict, Optional, Annotated
+
+class PatientData(BaseModel):
+    name: str
+    email: EmailStr
+    age: int
+    weight: float
+    married: bool
+    allergies: List[str]
+    contact_info: Dict[str,str]
+    
+    @field_validator('name')
+    @classmethod
+    def transform_name(cls, value):
+        return value.upper()
+    
+    @field_validator('email')
+    @classmethod
+    def email_validator(cls , value):
+        valid_domains =['hdfc.com','icici.com']
+        domain_name = value.split('@')[-1]
+        
+        if domain_name not in valid_domains:
+            raise ValueError('Not a valid domain')
+        return value
+    
+    def add_patient_data(patient: PatientData):
+        print(patient.name)
+        print(patient.email)
+        print(patient.age)
+        print(patient.weight)
+        print(patient.married)
+        print(patient.allergies)
+        print(patient.contact_info)
+    
+    patient_data = {
+        "name": "John",
+        "email": "john1458@gmail.com",
+        "linkedin_url":"https://www.linkedin.com",
+        "age": 25,
+        "weight": 51.25,
+        "married": True,
+        "allergies": ["peanuts","shellfish"],
+        "contact_info": {
+            "phone": "354-168-2458",
+        },}
+    
+    patient_1 = PatientData(**patient_data)
+    add_patient_data(patient_1)
